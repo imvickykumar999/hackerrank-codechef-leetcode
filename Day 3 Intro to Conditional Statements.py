a12bf0521be7f6df02445cd159df0b44c9d25733
imvickykumar999 <imvickykumar999@gmail.com>
2022-03-16 19:54:06 +0530

    day 2 and 3

---
+++ Day 3 Intro to Conditional Statements.py
@@ -0,0 +1,19 @@
+
+# https://www.hackerrank.com/challenges/30-conditional-statements/problem?isFullScreen=true
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    if N%2 != 0 or 6<=N<=20:
+        print('Weird')
+    else:
+        print('Not Weird')
