d6af8203bdb24feea99b15fa4670d6cdf598c2c9
imvickykumar999 <imvickykumar999@gmail.com>
2023-03-05 12:46:01 +0530

    sandclock

---
+++ Data Structure/2 Array - DS 2D SandClock.py
@@ -0,0 +1,37 @@
+
+# https://www.hackerrank.com/challenges/2d-array/problem?isFullScreen=true
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hourglassSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def hourglassSum(arr):
+    # Write your code here
+    pass
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    print(str(result) + '\n')
+
+    # fptr.write(str(result) + '\n')
+
+    # fptr.close()
